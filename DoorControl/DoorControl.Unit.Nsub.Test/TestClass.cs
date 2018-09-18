2be942dd886f133106ff8da9025e87bdb0c7f7fb
friis94 <nanna_friis@live.dk>
2018-09-18 09:26:48 +0200

    setup function

--- DoorControl/DoorControl.Unit.Nsub.Test/TestClass.cs
+++ DoorControl/DoorControl.Unit.Nsub.Test/TestClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace DoorControl.Unit.Nsub.Test
@@ -11,7 +12,20 @@ namespace DoorControl.Unit.Nsub.Test
     class TestClass
     {
         private DoorController _uut;
+        private IDoor _door;
+        private IAlarm _alarm;
+        private IEntryNotification _entryNotification;
+        private IUserValidation _userValidation;
 
+        [SetUp]
+        public void Setup()
+        {
+            _door = Substitute.For<IDoor>();
+            _alarm = Substitute.For<IAlarm>();
+            _entryNotification = Substitute.For<IEntryNotification>();
+            _userValidation = Substitute.For<IUserValidation>();
 
+            _uut = new DoorController(_alarm, _door, _entryNotification, _userValidation);
+        }
     }
 }
